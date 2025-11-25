2620f0cc7f214013bb4af67f00d5d555c7ec7a27
Rakesh <rakeshp22052005@gmail.com>
2025-11-25 18:42:29 +0530

    committed

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,55 @@
-const exp=require('express')
-const app=exp()
+const exp = require('express')
+const app = exp()
 require('dotenv').config()
-const mongoClient=require('mongodb').MongoClient
-const path=require('path')
-const { config } = require('dotenv')
+const mongoClient = require('mongodb').MongoClient
+const path = require('path')
 
-app.use(exp.static(path.join(__dirname,'../frontend/build')))
+// MIDDLEWARES
 app.use(exp.json())
 
+// ---- API ROUTES GO FIRST ----
+const userApp = require('./api/userApi')
+const comAdminApp = require('./api/comAdminApi')
+const adminApp = require('./api/adminApi')
+let securityApp = require('./api/securityApi')
+
+app.use('/user-api', userApp)
+app.use('/com-admin-api', comAdminApp)
+app.use('/admin-api', adminApp)
+app.use('/security-api', securityApp)
+
+
+// ---- STATIC FRONTEND AFTER API ROUTES ----
+app.use(exp.static(path.join(__dirname, '../frontend/build')))
+
+
+// ---- FALLBACK ONLY FOR NON-API ROUTES ----
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+
+
+// ---- DB CONNECTION ----
 mongoClient.connect(process.env.DB_URL)
-.then((client)=>{
-    const db=client.db('communityDb')
+.then((client) => {
+    const db = client.db('communityDb')
 
-    const usersCollection=db.collection('usersCollection')
-    const comAdminCollection=db.collection('comAdminCollection')
-    const adminCollection=db.collection('adminCollection')
+    const usersCollection = db.collection('usersCollection')
+    const comAdminCollection = db.collection('comAdminCollection')
+    const adminCollection = db.collection('adminCollection')
 
-    app.set('usersCollection',usersCollection)
-    app.set('comAdminCollection',comAdminCollection)
-    app.set('adminCollection',adminCollection)
+    app.set('usersCollection', usersCollection)
+    app.set('comAdminCollection', comAdminCollection)
+    app.set('adminCollection', adminCollection)
 
     console.log('DB Connection success')
 })
-.catch((err)=>console.log('Error in db connection',err))
+.catch((err) => console.log('Error in db connection', err))
 
-const userApp=require('./api/userApi')
-const comAdminApp=require('./api/comAdminApi')
-const adminApp=require('./api/adminApi')
-let securityApp=require('./api/securityApi')
 
-app.use('/user-api',userApp)
-app.use('/com-admin-api',comAdminApp)
-app.use('/admin-api',adminApp)
-app.use('/security-api',securityApp)
-
-app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+    res.status(500).send({ err })
 })
 
-app.use((err,req,res,next)=>{
-    res.send({
-        err:err
-    }) 
-})
-
-app.listen(4000,()=>console.log("Server running on port 4000..."))
+app.listen(4000, () => console.log("Server running on port 4000..."))
